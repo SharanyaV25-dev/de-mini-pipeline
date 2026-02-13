17041b8c24a22c862c656884f9233c935f90eb3c
SharanyaV25-dev <vsharanya00@gmail.com>
2026-02-13 20:59:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "root",
-    "database": "de_practice"
-}
+DB_HOST = "your_host"
+DB_PORT = 5432
+DB_NAME = "your_database"
+DB_USER = "your_username"
+DB_PASSWORD = "your_password"
+
+INPUT_CSV_PATH = "data/raw/input.csv"
+OUTPUT_JSON_PATH = "data/output/final.json"
